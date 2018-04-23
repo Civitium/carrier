e2a0dcf2515b309237f94307280fd11e1a004be3
Chris Lynch <Decoherence@users.noreply.github.com>
2018-04-23 18:38:25 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Carrier.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "~> 0.13"},
+      {:httpoison, "~> 1.0"},
       {:poison, "~> 2.0"},
       {:coverex, "~> 1.4.1", only: :test},
       {:ex_doc, "~> 0.10",  only: :dev}
